a676e28174d1d639691d102f8018a88527d04350
zethra <jediben97@gmail.com>
2020-04-01 18:18:20 -0400

    Added optimizing step to build.sh
    Signed-off-by: zethra <jediben97@gmail.com>

--- activities/DodgeTheCreeps.activity/build.sh
+++ activities/DodgeTheCreeps.activity/build.sh
@@ -2,8 +2,22 @@
 
 set -e
 
+if [ -d out ]; then
+    echo "Deleting out folder"
+    rm -r out
+fi
+
 cd game
 mkdir -p out
+echo "Exporting game..."
 godot --export HTML5 out/game.html
 cd ..
 mv game/out .
+
+if command -v wasm-opt >/dev/null 2>&1; then
+    echo "Optimizing wasm for size..."
+    wasm-opt out/game.wasm -o out/game.wasm -Oz
+    echo "Done"
+else
+    echo "Not optimizing wasm file, wasm-opt not installed"
+fi
