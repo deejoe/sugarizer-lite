31ef91167a4e6156e70f018db7aa3dfca164dbb5
jrtechs <jeffery@jrtechs.net>
2020-05-02 11:53:54 -0400

    added click handler for launch btn and created win scene

---
+++ activities/Fractionauts.activity/game/clickDetection.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		self.on_click()
+
+func on_click():
+	print("Click")
